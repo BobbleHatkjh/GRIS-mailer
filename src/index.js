727cb1285f463438c7817304e72833a202bcee0c
BobbleHatkjh <3206633623@qq.com>
2021-05-02 18:37:55 +0800

    · 修改了标准邮件

--- src/index.js
+++ src/index.js
@@ -8,9 +8,17 @@ Koa.Listen(8010);
 Koa.GET((param) => {
     let url = param.url;
     let request = param.request;
-    // let token = param.token;
+    let token = param.token;
     let query = request.query;
     console.log(url,'============\n', param.request.body, param.method);
+    if (param.method !== 'POST') return {
+        status: 403,
+        msg: '使用了错误的请求方式，请使用POST方式请求'
+    }
+    if (token !== 'POST') return {
+        status: 403,
+        msg: '使用了错误的请求方式，请使用POST方式请求'
+    }
     return {
         status: 200,
         msg: {
